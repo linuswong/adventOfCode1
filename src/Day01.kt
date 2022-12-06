24cbff6011c67844327520db87cef1b1e248dbf3
linuswong <linuswong9@gmail.com>
2022-12-06 11:58:26 -0800

    attempt#1 day 2
    bingbong

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,21 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var counter: Int=0
+        
+        for(i in input){
+         if(i[0]=="A"){
+             counter+=8
+        }
+         else if(i[0]=="B"){
+             counter++
+         }
+         
+         else if(i[0]=="C"){
+         counter+=6
+         }
+        }
+        
+        return counter
     }
 
     fun part2(input: List<String>): Int {
@@ -8,10 +23,10 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
+    val testInput = readInput("Day02_test")
     check(part1(testInput) == 1)
 
-    val input = readInput("Day01")
+    val input = readInput("Day02")
     println(part1(input))
     println(part2(input))
 }
